b33695213017901e6c20e01a9fd22f3c007e5def
TanDung <dungduong241101@gmail.com>
2022-11-11 17:46:11 +0700

    11/11/2022

--- app/__init__.py
+++ app/__init__.py
@@ -5,3 +5,11 @@ app = Flask(__name__)
 from app import routes
 #app.config["SECRET_KEY"] = "hard-secret-key"
 app.config.from_object(Config)
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
+
+app.app_context().push()
+from app import models
